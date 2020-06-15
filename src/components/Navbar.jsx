84c2e5c7cb93049d25014eff61819ec2f23ac3a8
iamdeepz <deepakchandrasekaran14@gmail.com>
2020-06-15 19:28:23 +0530

    started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="nav-bar">
+            <div className="container">
+                <h2>React FormValidation</h2>
+            </div>
+        </nav>
+    )
+}
